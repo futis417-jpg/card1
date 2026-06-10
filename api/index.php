b3857d99a7ecfbd046edd8686136a7b8394f78b9
futis417-jpg <futis417@gmail.com>
2026-06-10 17:29:06 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
-// Archivo puente para Vercel
+// Mapeo automático de rutas para Vercel
+function fix_paths($buffer) {
+    return str_replace('/home/arturo/www/', __DIR__ . '/../', $buffer);
+}
+
+// Empezamos a capturar la salida para evitar errores de cabeceras
+ob_start('fix_paths');
+
+// Cargamos el archivo original
 require_once __DIR__ . '/../index.php';
 ?>
